8b1f923268e5b59deebac879bdda1e4640ece6e1
Iden Craven <iden.craven@gmail.com>
2020-02-13 14:38:27 -0700

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -22,8 +22,8 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 """
-from collections import OrderedDict
 from __future__ import absolute_import, unicode_literals, print_function
+from collections import OrderedDict
 
 import io
 import os
